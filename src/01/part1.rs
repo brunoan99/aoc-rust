f09dde6256f7ef29e01bb06a6ee65a6c881ddd43
Bruno Andrade <brunoan99@gmail.com>
2023-12-02 14:36:13 -0300

    feat: day 01

--- src/01/part1.rs
+++ src/01/part1.rs
@@ -3,12 +3,28 @@ pub fn exec() -> () {
   let output = process(input);
   println!("Day 01 - Part 01 - {output}");
 }
-
-fn process(input: &str) -> String {
-  "The solution should be here".into()
+fn process(input: &str) -> i32 {
+  input
+    .lines()
+    .filter_map(|line| {
+      let digits = line.chars().filter(|n| n.is_digit(10)).collect::<Vec<_>>();
+      let first = digits.first()?;
+      let last = digits.last()?;
+      format!("{first}{last}").parse::<i32>().ok()
+    })
+    .sum()
 }
 
 #[cfg(test)]
-mod tests {
-  // use super::*;
+mod testsday01part01 {
+  use super::*;
+
+  #[test]
+  pub fn example() {
+    let input = "1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet";
+    assert_eq!(process(input), 142)
+  }
 }
--- src/01/part2.rs
+++ src/01/part2.rs
@@ -1,14 +1,55 @@
 pub fn exec() {
-  let input = include_str!("input1.txt");
+  let input = include_str!("input2.txt");
   let output = process(input);
   println!("Day 01 - Part 02 - {output}");
 }
 
-fn process(input: &str) -> String {
-  "The solution should be here".into()
+fn process(input: &str) -> i32 {
+  input
+    .lines()
+    .map(|line| {
+      line
+        .replace("zerone", "01")
+        .replace("oneight", "18")
+        .replace("twone", "21")
+        .replace("threeight", "38")
+        .replace("fiveight", "58")
+        .replace("eightwo", "82")
+        .replace("eighthree", "83")
+        .replace("nineight", "98")
+        .replace("zero", "0")
+        .replace("one", "1")
+        .replace("two", "2")
+        .replace("three", "3")
+        .replace("four", "4")
+        .replace("five", "5")
+        .replace("six", "6")
+        .replace("seven", "7")
+        .replace("eight", "8")
+        .replace("nine", "9")
+    })
+    .filter_map(|line| {
+      let digits = line.chars().filter(|n| n.is_digit(10)).collect::<Vec<_>>();
+      let first = digits.first()?;
+      let last = digits.last()?;
+      format!("{first}{last}").parse::<i32>().ok()
+    })
+    .sum()
 }
 
 #[cfg(test)]
-mod tests {
-  // use super::*;
+mod testsday01part02 {
+  use super::*;
+
+  #[test]
+  pub fn example() {
+    let input = "two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen";
+    assert_eq!(process(input), 281)
+  }
 }
