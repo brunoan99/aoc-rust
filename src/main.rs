c695e435d49bb7b20d1c28fda63031d5ee7975a9
Bruno Andrade <brunoan99@gmail.com>
2023-12-09 22:36:04 -0300

    refactor: simplify day 07 solution

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 fn main() -> () {
-  // mylib::day01part01::exec();
-  // mylib::day01part02::exec();
-  // mylib::day02part01::exec();
-  // mylib::day02part02::exec();
-  // mylib::day03part01::exec();
-  // mylib::day03part02::exec();
-  // mylib::day04part01::exec();
-  // mylib::day04part02::exec();
-  // mylib::day05part01::exec();
-  // mylib::day05part02::exec();
-  // mylib::day06part01::exec();
-  // mylib::day06part02::exec();
+  mylib::day01part01::exec();
+  mylib::day01part02::exec();
+  println!("");
+  mylib::day02part01::exec();
+  mylib::day02part02::exec();
+  println!("");
+  mylib::day03part01::exec();
+  mylib::day03part02::exec();
+  println!("");
+  mylib::day04part01::exec();
+  mylib::day04part02::exec();
+  println!("");
+  mylib::day05part01::exec();
+  mylib::day05part02::exec();
+  println!("");
+  mylib::day06part01::exec();
+  mylib::day06part02::exec();
+  println!("");
   mylib::day07part01::exec();
   mylib::day07part02::exec();
+  println!("");
 }
