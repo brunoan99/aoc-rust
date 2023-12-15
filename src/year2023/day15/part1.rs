e5fa152021cb97132f8d8cf51d220af3ab1d4484
Bruno Andrade <brunoan99@gmail.com>
2023-12-15 06:13:24 -0300

    feat: day 15

--- src/year2023/day15/part1.rs
+++ src/year2023/day15/part1.rs
@@ -2,9 +2,57 @@ pub fn exec(input: &str) -> String {
   process(input).to_string()
 }
 
+#[derive(Debug)]
+struct Puzzle {
+  hashs: Vec<Vec<char>>,
+}
+
+impl From<&str> for Puzzle {
+  fn from(value: &str) -> Self {
+    let hashs = value
+      .trim()
+      .split(',')
+      .map(|line| line.trim().chars().collect())
+      .collect();
+    Self { hashs }
+  }
+}
+
+#[derive(Debug)]
+struct PuzzleASCII {
+  hashs: Vec<Vec<usize>>,
+}
+
+impl From<Puzzle> for PuzzleASCII {
+  fn from(value: Puzzle) -> Self {
+    let hashs = value
+      .hashs
+      .into_iter()
+      .map(|line| {
+        line
+          .into_iter()
+          .map(|char| char as usize)
+          .collect::<Vec<usize>>()
+      })
+      .collect::<Vec<Vec<usize>>>();
+    Self { hashs }
+  }
+}
+
+impl PuzzleASCII {
+  fn solve(self) -> usize {
+    self
+      .hashs
+      .into_iter()
+      .map(|hash| hash.into_iter().fold(0, |acc, v| ((acc + v) * 17) % 256))
+      .sum()
+  }
+}
+
 fn process(input: &str) -> usize {
-  let _ = input;
-  0
+  let puzzle = Puzzle::from(input);
+  let puzzle_ascii = PuzzleASCII::from(puzzle);
+  puzzle_ascii.solve()
 }
 
 #[cfg(test)]
@@ -12,8 +60,14 @@ mod test_day15_part01_example {
   use super::*;
 
   #[test]
-  pub fn example() {
-    let input = "";
-    assert_eq!(process(input), 0);
+  pub fn example_1() {
+    let input = "HASH";
+    assert_eq!(process(input), 52);
+  }
+
+  #[test]
+  pub fn example_2() {
+    let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+    assert_eq!(process(input), 1320);
   }
 }
--- src/year2023/day15/part2.rs
+++ src/year2023/day15/part2.rs
@@ -2,9 +2,139 @@ pub fn exec(input: &str) -> String {
   process(input).to_string()
 }
 
+#[derive(Debug)]
+struct Puzzle {
+  hashs: Vec<String>,
+}
+
+impl From<&str> for Puzzle {
+  fn from(value: &str) -> Self {
+    let hashs = value
+      .trim()
+      .split(',')
+      .map(|line| line.trim().to_owned())
+      .collect();
+    Self { hashs }
+  }
+}
+
+#[derive(Debug)]
+struct BoxOP {
+  label: Vec<char>,
+  op: char,
+  focal_length: usize,
+}
+
+impl From<&str> for BoxOP {
+  fn from(value: &str) -> Self {
+    if value.contains("-") {
+      Self {
+        label: value.split_once("-").unwrap().0.chars().collect(),
+        op: '-',
+        focal_length: 0,
+      }
+    } else {
+      let (label_str, focal_length_str) = value.split_once("=").unwrap();
+      Self {
+        label: label_str.chars().collect(),
+        op: '=',
+        focal_length: focal_length_str.parse().unwrap(),
+      }
+    }
+  }
+}
+
+impl BoxOP {
+  fn get_pos(&self) -> usize {
+    self
+      .label
+      .iter()
+      .fold(0, |acc, v| ((acc + v.clone() as usize) * 17) % 256)
+  }
+}
+
+#[derive(Debug, Clone)]
+struct Len {
+  label: Vec<char>,
+  focal_length: usize,
+}
+
+#[derive(Debug, Default, Clone)]
+struct Box {
+  lens: Vec<Len>,
+}
+
+#[derive(Debug)]
+struct Slots {
+  boxes: Vec<Box>,
+}
+
+impl From<Puzzle> for Slots {
+  fn from(value: Puzzle) -> Self {
+    let box_ops = value
+      .hashs
+      .into_iter()
+      .map(|hash| BoxOP::from(hash.as_ref()))
+      .collect::<Vec<BoxOP>>();
+
+    let mut boxes: Vec<Box> = vec![Box { lens: vec![] }; 256];
+    for box_op in box_ops.into_iter() {
+      match box_op.op {
+        '-' => {
+          let pos = box_op.get_pos();
+          let pos_in_lens_op = boxes[pos]
+            .lens
+            .iter()
+            .position(|len| len.label == box_op.label);
+          if let Some(pos_in_lens) = pos_in_lens_op {
+            boxes[pos].lens.remove(pos_in_lens);
+          }
+        }
+        '=' => {
+          let pos = box_op.get_pos();
+          let pos_in_lens_op = boxes[pos]
+            .lens
+            .iter()
+            .position(|len| len.label == box_op.label);
+          if let Some(pos_in_lens) = pos_in_lens_op {
+            boxes[pos].lens[pos_in_lens].focal_length = box_op.focal_length;
+          } else {
+            boxes[pos].lens.push(Len {
+              label: box_op.label,
+              focal_length: box_op.focal_length,
+            });
+          }
+        }
+        _ => panic!("Unsuported operation"),
+      }
+    }
+
+    Self { boxes }
+  }
+}
+
+impl Slots {
+  fn eval(self) -> usize {
+    self
+      .boxes
+      .into_iter()
+      .enumerate()
+      .map(|(pos, b)| {
+        b.lens
+          .into_iter()
+          .enumerate()
+          .map(|(pos, len)| (pos + 1) * len.focal_length)
+          .sum::<usize>()
+          * (pos + 1)
+      })
+      .sum()
+  }
+}
+
 fn process(input: &str) -> usize {
-  let _ = input;
-  0
+  let puzzle = Puzzle::from(input);
+  let slots = Slots::from(puzzle);
+  slots.eval()
 }
 
 #[cfg(test)]
@@ -12,8 +142,8 @@ mod test_day15_part02_example {
   use super::*;
 
   #[test]
-  pub fn example() {
-    let input = "";
-    assert_eq!(process(input), 0);
+  pub fn example_2() {
+    let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+    assert_eq!(process(input), 145);
   }
 }
